811e6838c18657cb6a97a41d40c2ad329d1e4985
Santiago <santo1996.29@gmail.com>
2024-04-23 09:03:31 -0400

    define component for headings

---
+++ components/Heading.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  level: 1 | 2
+}
+
+const props = defineProps<Props>()
+
+const attrs = useAttrs()
+
+const cssClass = [{
+  'text-6xl': props.level === 1,
+  'text-5xl': props.level === 2,
+}, 'text-green-400', 'text-bold', attrs.class]
+</script>
+
+<template>
+  <h1 v-if="level === 1" :class="cssClass"><slot></slot></h1>
+  <h2 v-if="level === 2" :class="cssClass"><slot></slot></h2>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="px-16">
-    <h1 class="text-6xl font-jersey text-center text-green-400 mb-8">Santiago Rodríguez</h1>
-    <div class="md:flex">
-      <div class="w-full h-72 bg-red-500 md:mr-6"></div>
-      <div class="mt-8 md:mt-0">
-        <p class="mb-6 text-xl">
-          I am <strong>THE WEB DEVELOPER</strong> full of ideas that's
-          eager to work on your projects.
-        </p>
-        <p class="mb-6 text-xl">
-          If you want a web site/app that follows
-          the rules of proper UI/UX and that
-          gets deployed quickly, <strong>I'm the guy</strong>.
-        </p>
-        <p class="text-xl">
-          I have <strong>years of experience</strong> developing
-          web solutions for the problems of the
-          modern bussiness.
-        </p>
+    <main>
+      <Heading :level="1" class="mb-12 text-center">Santiago Rodríguez</Heading>
+      <div class="md:flex">
+        <div class="w-full h-72 bg-red-500 md:mr-6"></div>
+        <div class="mt-8 md:mt-0">
+          <p class="mb-6 text-xl">
+            I am <strong>THE WEB DEVELOPER</strong> full of ideas that's
+            eager to work on your projects.
+          </p>
+          <p class="mb-6 text-xl">
+            If you want a web site/app that follows
+            the rules of proper UI/UX and that
+            gets deployed quickly, <strong>I'm the guy</strong>.
+          </p>
+          <p class="text-xl">
+            I have <strong>years of experience</strong> developing
+            web solutions for the problems of the
+            modern bussiness.
+          </p>
+        </div>
       </div>
-    </div>
+    </main>
+    <section class="mt-16">
+      <Heading :level="2" class="text-center">Toolset</Heading>
+    </section>
   </div>
 </template>
