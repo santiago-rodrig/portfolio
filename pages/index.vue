e14342606ad486391ad0f0b686cb2fdb67bf502d
Santiago <santo1996.29@gmail.com>
2024-04-28 14:18:42 -0400

    make landing page text better

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,28 @@
 <template>
-  <div class="px-16">
-    <main>
+  <div class="px-12">
+    <main class="min-h-screen pb-12">
       <Heading :level="1" class="mb-12 text-center">Santiago Rodriguez</Heading>
-      <div class="md:flex items-start">
-        <img :style="{ maxWidth: '210px' }" class="md:w-1/3 mx-auto" src="~/assets/img/full-body-person.png"
-          alt="Man looking forward">
-        <div class="mt-8 md:mt-0 md:w-2/3">
-          <p class="mb-6 text-xl">
-            I am <strong>THE WEB DEVELOPER</strong> full of ideas that's
+      <div class="flex flex-col md:flex-row items-center">
+        <div class="mt-8 md:mt-0 md:w-2/3 order-2 md:order-1">
+          <p class="text-2xl">
+            I am <br><br><strong>THE WEB DEVELOPER</strong><br><br> full of ideas that's
             eager to work on your projects.
-          </p>
-          <p class="mb-6 text-xl">
-            If you want a web site/app that follows
-            the rules of proper UI/UX and that
-            gets deployed quickly, <strong>I'm the guy</strong>.
-          </p>
-          <p class="text-xl">
-            I have <strong>years of experience</strong> developing
+
+            I have <br><br><strong>years of experience</strong><br><br> developing
             web solutions for the problems of the
             modern bussiness.
+
+            If you want a web site/app that follows
+            the rules of proper UI/UX and that
+            gets deployed quickly, <br><br><strong>I'm the guy</strong>.
+            
           </p>
         </div>
+        <img :style="{ maxWidth: '210px' }" class="md:w-1/3 mx-auto order-1 md:order-2" src="~/assets/img/full-body-person.png"
+          alt="Man looking forward">
       </div>
     </main>
-    <section class="mt-12">
+    <section class="min-h-screen">
       <Heading :level="2" class="text-center mb-8">Toolset</Heading>
       <div class="md:flex items-start">
         <img class="md:mr-6 md:w-1/3" :style="{ maxWidth: 'min(612px, 100%)' }" src="~/assets/img/toolbox.png" alt="Toolbox">
